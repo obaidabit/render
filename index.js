c42a18b9017973483fcc6cabb978a21f10e0ecd9
obedabit <obitbit30@gmail.com>
2023-10-08 16:04:56 +0300

    change cron

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const nodeCron = require("node-cron");
 
 const app = express();
-
+process.env.TZ = "Etc/GMT-3";
 app.get("/", (req, res) => {
-  console.log("Starting node cron");
-  const task = nodeCron.schedule("* * * * *", (d) => {
-    console.log("Running a task every minute");
-    console.log(new Date());
-  });
   res.json({
     msg: "Hello World",
     tasks: nodeCron.getTasks(),
   });
+});
+
+console.log("Starting node cron");
+nodeCron.schedule("* * * * *", (d) => {
+  console.log("Running a task every minute");
+  console.log(new Date());
 });
 
 app.listen(3000, () => {
